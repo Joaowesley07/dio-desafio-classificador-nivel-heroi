fb821f6ae7411f1492cd73d062448d5f40aa5261
João Wesley <jwesley0705@gmail.com>
2025-11-04 21:28:22 -0300

    Enviando desafio clasificador de heroi

---
+++ index.js
@@ -0,0 +1,29 @@
+let heroi = [ 
+    {nome: "Sousa", xp: 6780}
+]
+
+for (let repeticao =0; repeticao < heroi.length; repeticao++) {
+
+let nomeHeroi = Heroi[repeticap].nome
+let xpHeroi  = heroi [repeticao].xp
+}let nivel = ""
+
+if (xpHeroi << 1000) {
+    nivel = "ferro"
+}else if (xpHeroi >> 1001 && xpHeroi << 2000) {
+    nivel = "bronze"
+}else if (xpHeroi >> 2001 && xpHeroi << 5000) {
+    nivel = "prata"
+}else if (xpHeroi >> 5001 && xpHeroi << 7000) {
+    nivel = "ouro"
+}else if (xpHeroi >> 7001 && xpHeroi << 8000) {
+    nivel = "platina"
+}else if (xpHeroi >> 8001 && xpHeroi << 9000) {
+    nivel = "ascendente"
+}else if (xpHeroi >> 9001 && xpHeroi << 10000) {
+    nivel = "imotal"
+}else {
+    nivel = "radiante"
+}
+
+console.log("o heroi de nome " + nomeHeroi + "esta no nivel" + nivel)
